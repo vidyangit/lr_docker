78769acb8117e67f8fdc396f98e63297c166b34a
Vidya <kammavidya@gmail.com>
2026-03-05 12:17:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Sample data
+X = np.array([[1], [2], [3], [4]])
+y = np.array([2, 4, 6, 8])
+
+model = LinearRegression()
+model.fit(X, y)
+
+print("Coefficient:", model.coef_)
+print("Intercept:", model.intercept_)
