f451ca60f9907a863d817a6ddb466ebcdab65599
Vidya <kammavidya@gmail.com>
2026-03-05 12:55:14 +0530

    updated testcases

--- app.py
+++ app.py
@@ -1,33 +1,12 @@
-#from sklearn.linear_model import LinearRegression
-#import numpy as np
-# Sample data
-#X = np.array([[1], [2], [3], [4]])
-#y = np.array([2, 4, 6, 8])
-#model = LinearRegression()
-#model.fit(X, y)
-#print("DevOps CI Pipeline Working Again")
-#print("Coefficient:", model.coef_)
-#print("Intercept:", model.intercept_)
-
-
-import numpy as np
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
+# Sample data
 X = np.array([[1], [2], [3], [4]])
 y = np.array([2, 4, 6, 8])
 
-def test_coefficient():
-    model = LinearRegression()
-    model.fit(X, y)
-    assert round(model.coef_[0], 2) == 2.0
-
-def test_intercept():
-    model = LinearRegression()
-    model.fit(X, y)
-    assert round(model.intercept_, 2) == 0.0
-
-def test_prediction():
-    model = LinearRegression()
-    model.fit(X, y)
-    pred = model.predict([[5]])
-    assert round(pred[0], 2) == 10.0
+model = LinearRegression()
+model.fit(X, y)
+print("DevOps CI Pipeline Working Again")
+print("Coefficient:", model.coef_)
+print("Intercept:", model.intercept_)
